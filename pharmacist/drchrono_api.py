9aaf42b72e15fda1a052e2394c28e298403cc62b
sean <sean@hella.rip>
2016-09-08 15:50:36 -0700

    add logging for email clickthrough events

--- pharmacist/drchrono_api.py
+++ pharmacist/drchrono_api.py
@@ -32,76 +32,13 @@ class DrchronoAPI:
                 Medication().save_from_dict(item)
             url = data['next']
 
-
-endpoint_models = {
-    'patients': Patient,
-    'medications': Medication,
-    }
-
-
-def get_one(user, endpoint, item_id, parameters={}):
-    # First, try to retrieve data from our DB if we've retrieved it from the API previously
-    model = endpoint_models[endpoint]
-    try:
-        data = model.objects.get(item_id=item_id, **parameters)
-    except:
-        # Get it from the API directly
-        # TODO: wrap this in an error-handler / logger
-        social = user.social_auth.get(user=user)
-        access_token = social.extra_data['access_token']
-        headers = {'Authorization': 'Bearer {0}'.format(access_token)}
-        url = 'https://drchrono.com/api/{0}/{1}?{2}'.format(endpoint, item_id, urllib.urlencode(parameters))
-        data = requests.get(url, headers=headers).json()
-
-        # Save the item in the DB for future reference
-        new_model = model()
-        new_model.save_from_dict(data)
-        return new_model
-
-    return data
-
-def get_all(user, endpoint, parameters={}, patient=None):
-    # Try the DB first for data
-    model = endpoint_models[endpoint]
-
-    all_objs = model.objects
-    if patient:
-        patient = Patient.objects.get(item_id=patient)
-        all_objs = all_objs.filter(patient=patient)
-
-    if not all_objs.exists():
-        all_objs = []
-        # Get it from the API directly
-        # TODO: wrap this in an error-handler / logger
-        social = user.social_auth.get(user=user)
-        access_token = social.extra_data['access_token']
-        headers = {'Authorization': 'Bearer {0}'.format(access_token)}
-        url = 'https://drchrono.com/api/{0}?{1}'.format(endpoint, urllib.urlencode(parameters))
-        json_objs = []
-
-        # Fetch the objects one "page" at a time
-        while url:
-            data = requests.get(url, headers=headers).json()
-            json_objs.extend(data['results'])
-            url = data['next'] # A JSON null on the last page
-
-        for item in json_objs:
-            new_model = model()
-            new_model.save_from_dict(item)
-            all_objs.append(new_model)
-
-    return all_objs
-
-def update(user, endpoint, item_id, parameters):
-    social = user.social_auth.get(user=user)
-    access_token = social.extra_data['access_token']
-    headers = {'Authorization': 'Bearer {0}'.format(access_token)}
-    if item_id:
-        url = 'https://drchrono.com/api/{0}/{1}'.format(endpoint, item_id)
-    else:
-        url = 'https://drchrono.com/api/{0}'.format(endpoint)
-    data = requests.patch(url, headers=headers, data=parameters).text
-    return data
+    def _patch(self, user, endpoint, item_id, parameters):
+        if item_id:
+            url = 'https://drchrono.com/api/{0}/{1}'.format(endpoint, item_id)
+        else:
+            url = 'https://drchrono.com/api/{0}'.format(endpoint)
+        data = requests.patch(url, headers=self.headers, data=parameters).text
+        return data
 
 def dispense_med(user, form, med_id, qty):
     #med = get_one(user, 'medications', med_id)
